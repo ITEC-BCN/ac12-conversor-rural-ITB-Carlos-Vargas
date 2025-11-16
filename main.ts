617fcb1399a11a211b73b68b46aacd9c473a8eae
Carlos Vargas <carlos.vargas.7e8@itb.cat>
2025-11-16 19:24:15 +0100

    animación de talado listo

--- main.ts
+++ main.ts
@@ -1,45 +1,53 @@
-//  Presiona izquierda
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    woodCutter,
+    assets.animation`talar`,
+    100,
+    false
+    )
+})
+// Presiona izquierda
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     walk_lef()
 })
-//  Suelta derecha
-controller.right.onEvent(ControllerButtonEvent.Released, function on_right_released() {
+// Suelta derecha
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
     stop_walk_anim()
 })
-//  Suelta izquierda
-controller.left.onEvent(ControllerButtonEvent.Released, function on_left_released() {
+// Suelta izquierda
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
     stop_walk_anim()
 })
-function walk_lef() {
-    animation.runImageAnimation(woodCutter, assets.animation`
-            left_walk
-            `, 200, true)
+function walk_lef () {
+    animation.runImageAnimation(
+    woodCutter,
+    assets.animation`left_walk`,
+    200,
+    true
+    )
 }
-
-//  --- Eventos del mando ---
-//  Presiona derecha
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+// --- Eventos del mando ---
+// Presiona derecha
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     walk_right()
 })
-function stop_walk_anim() {
+function stop_walk_anim () {
     animation.stopAnimation(animation.AnimationTypes.All, woodCutter)
-    woodCutter.setImage(assets.image`
-        myImage
-        `)
+    woodCutter.setImage(assets.image`myImage`)
 }
-
-//  --- Funciones para animación ---
-function walk_right() {
-    animation.runImageAnimation(woodCutter, assets.animation`
-            rigth_walk
-            `, 200, true)
+// --- Funciones para animación ---
+function walk_right () {
+    animation.runImageAnimation(
+    woodCutter,
+    assets.animation`rigth_walk`,
+    200,
+    true
+    )
 }
-
-let woodCutter : Sprite = null
-scene.setBackgroundImage(assets.image`
-    bosc
-    `)
-woodCutter = sprites.create(assets.image`
-    myImage
-    `, SpriteKind.Player)
+let woodCutter: Sprite = null
+scene.setBackgroundImage(assets.image`bosc`)
+let tree = sprites.create(assets.image`myImage0`, SpriteKind.Enemy)
+woodCutter = sprites.create(assets.image`myImage`, SpriteKind.Player)
+tree.setPosition(46, 94)
+woodCutter.setPosition(18, 97)
 controller.moveSprite(woodCutter)
