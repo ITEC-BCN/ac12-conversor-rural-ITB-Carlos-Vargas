83c0a7f315769b7ddd5e262a3b862a55c8025b29
Carlos Vargas <carlos.vargas.7e8@itb.cat>
2025-11-16 18:03:23 +0100

    movimiento derechaizquierda realizados

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+# Presiona izquierda
+
+def on_left_pressed():
+    walk_lef()
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+# Suelta derecha
+
+def on_right_released():
+    stop_walk_anim()
+controller.right.on_event(ControllerButtonEvent.RELEASED, on_right_released)
+
+# Suelta izquierda
+
+def on_left_released():
+    stop_walk_anim()
+controller.left.on_event(ControllerButtonEvent.RELEASED, on_left_released)
+
+def walk_lef():
+    animation.run_image_animation(woodCutter,
+        assets.animation("""
+            left_walk
+            """),
+        200,
+        True)
+# --- Eventos del mando ---
+# Presiona derecha
+
+def on_right_pressed():
+    walk_right()
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def stop_walk_anim():
+    animation.stop_animation(animation.AnimationTypes.ALL, woodCutter)
+    woodCutter.set_image(assets.image("""
+        myImage
+        """))
+# --- Funciones para animación ---
+def walk_right():
+    animation.run_image_animation(woodCutter,
+        assets.animation("""
+            rigth_walk
+            """),
+        200,
+        True)
+woodCutter: Sprite = None
 scene.set_background_image(assets.image("""
     bosc
     """))
@@ -5,9 +51,3 @@ woodCutter = sprites.create(assets.image("""
     myImage
     """), SpriteKind.player)
 controller.move_sprite(woodCutter)
-animation.run_image_animation(woodCutter,
-    assets.animation("""
-        myAnim
-        """),
-    200,
-    True)
--- main.ts
+++ main.ts
@@ -1,9 +1,45 @@
-scene.setBackgroundImage(assets.image`bosc`)
-let woodCutter = sprites.create(assets.image`myImage`, SpriteKind.Player)
+//  Presiona izquierda
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    walk_lef()
+})
+//  Suelta derecha
+controller.right.onEvent(ControllerButtonEvent.Released, function on_right_released() {
+    stop_walk_anim()
+})
+//  Suelta izquierda
+controller.left.onEvent(ControllerButtonEvent.Released, function on_left_released() {
+    stop_walk_anim()
+})
+function walk_lef() {
+    animation.runImageAnimation(woodCutter, assets.animation`
+            left_walk
+            `, 200, true)
+}
+
+//  --- Eventos del mando ---
+//  Presiona derecha
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    walk_right()
+})
+function stop_walk_anim() {
+    animation.stopAnimation(animation.AnimationTypes.All, woodCutter)
+    woodCutter.setImage(assets.image`
+        myImage
+        `)
+}
+
+//  --- Funciones para animación ---
+function walk_right() {
+    animation.runImageAnimation(woodCutter, assets.animation`
+            rigth_walk
+            `, 200, true)
+}
+
+let woodCutter : Sprite = null
+scene.setBackgroundImage(assets.image`
+    bosc
+    `)
+woodCutter = sprites.create(assets.image`
+    myImage
+    `, SpriteKind.Player)
 controller.moveSprite(woodCutter)
-animation.runImageAnimation(
-woodCutter,
-assets.animation`myAnim`,
-200,
-true
-)
