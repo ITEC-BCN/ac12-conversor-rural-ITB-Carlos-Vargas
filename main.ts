57f1dec58a231340c38cad2c779d816107c83a0b
Carlos Vargas <carlos.vargas.7e8@itb.cat>
2025-11-18 11:44:33 +0100

    assets del menu listos

--- main.ts
+++ main.ts
@@ -56,57 +56,82 @@ function stop_walk_anim () {
     woodCutter.setImage(assets.image`myImage`)
 }
 function open_main_menu () {
-    backPack = miniMenu.createMenuFromArray([miniMenu.createMenuItem("huevo", img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . b b b b . . . . . . 
-        . . . . b b 1 1 1 1 b b . . . . 
-        . . . . b 1 1 1 3 3 1 b . . . . 
-        . . . b 1 1 1 1 3 3 3 1 b . . . 
-        . . . b 1 1 3 1 1 3 3 1 b . . . 
-        . . b d 1 1 1 1 1 1 1 1 d b . . 
-        . . b d 3 3 1 1 1 1 1 1 d b . . 
-        . . b b 3 3 1 1 1 1 3 3 d b . . 
-        . . c b b d 1 1 1 3 3 b d c . . 
-        . . c d d d d d d b b b d c . . 
-        . . c b d d b b d b b d b c . . 
-        . . . c d d b b d d d d c . . . 
-        . . . . c b d d d d b c . . . . 
-        . . . . . c c c c c c . . . . . 
-        `), miniMenu.createMenuItem("pollo", img`
-        ..........bbbbbb................
-        .......bbb444444bb..............
-        .....2244444ddd444b.............
-        ....244444444dddd44e............
-        ...244444444444ddd4be...........
-        ..244444444444444d44be..........
-        .2b444444444444444d4be..........
-        .2b44444444444444444bbe.........
-        2bbb4444444444444444bbe.........
-        2bbb4444444444444444bbe.........
-        2bb4b4444444444444444bbe........
-        2bb4444444444444444444be........
-        2bb44444444444444444444e........
-        2bbb444bbb4444444444444e........
-        22bbb444bb4bb444444444be........
-        .2bbbbb44bbbb44444444bbe........
-        .22bbbbbbbb44bbb444444bbe.......
-        ..eeebbbbbbb44bbb444444be.......
-        ...eeeeebbbbbbbb44b4444be.......
-        .....eeeeee222bb44bbb4bbe.......
-        .......eeeee222bb44bbbbee.......
-        ............e222bbbbbbbec.......
-        ..............ee2bbbbeebdb......
-        .................eeeeecdddb.....
-        .......................cd11bbbb.
-        ........................cd111dbb
-        .........................b11111c
-        .........................c11dd1c
-        .........................cd1dbc.
-        .........................cb11c..
-        ..........................ccc...
-        ................................
-        `)])
+    backPack = [
+    miniMenu.createMenuItem("huevo", img`
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 b b 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 c c 1 1 1 1 1 1 1 
+        1 1 1 1 1 d c d d c 1 1 1 1 1 1 
+        1 1 1 1 c c d d d d c b 1 1 1 1 
+        1 1 1 c d d d 1 d d d b c 1 1 1 
+        1 1 1 c d d 1 d d d d b c 1 1 1 
+        1 1 c d d d d d d d d d d c 1 1 
+        1 1 c d d d d d d d d d d c 1 1 
+        1 1 c d d d d d d d d d b c 1 1 
+        1 1 c d d d d d d d d b b c 1 1 
+        1 1 1 c d d d d d d d e c 1 1 1 
+        1 1 1 1 c c b b d d c b 1 1 1 1 
+        1 1 1 1 1 d c c c c 1 1 1 1 1 1 
+        1 1 1 1 1 d b b b b 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        `),
+    miniMenu.createMenuItem("pollo", img`
+        1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 f 2 f 1 1 1 1 1 1 1 
+        1 1 1 1 f f 2 2 2 f f 1 1 1 1 1 
+        1 1 1 1 f 2 1 1 1 1 1 f 1 1 1 1 
+        1 1 1 1 f 1 1 1 1 1 1 1 f 1 1 1 
+        1 1 1 1 f 1 f 1 1 5 5 f f 1 1 1 
+        1 1 f 1 f 1 1 1 1 2 1 1 f 1 1 1 
+        1 f 1 f f b 1 1 1 1 1 1 1 f 1 1 
+        1 f 1 1 b b b 1 1 1 1 1 1 f 1 1 
+        1 f 1 b b b b b 1 1 1 1 1 b f 1 
+        1 f 1 b b b b b 1 1 1 1 1 b f 1 
+        1 f 1 1 b b b 1 1 1 1 1 1 b f 1 
+        1 1 f 1 1 1 1 1 1 1 1 1 1 f 1 1 
+        1 1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 
+        1 1 1 1 f f 5 f f 5 f f 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        `),
+    miniMenu.createMenuItem("cabra", img`
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 b 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 b 1 c 1 1 1 1 
+        1 1 1 1 d d d d 1 b d b 1 d d d 
+        1 1 1 1 1 b 1 1 d 1 f d d f d 1 
+        1 1 1 1 1 1 d d d d d d 1 b d d 
+        1 1 1 1 1 1 d d d b d d d 1 b b 
+        1 1 1 1 1 1 d d d 1 d d d b b b 
+        d d 1 1 1 1 1 1 1 1 1 b 1 1 b 1 
+        d d 1 1 1 1 1 1 d 1 1 b 1 1 b 1 
+        d d 1 1 1 b d d d 1 b b 1 1 b 1 
+        b d d 1 1 b d d d 1 b b 1 1 1 1 
+        1 d d b 1 b d d d 1 b b 1 1 1 1 
+        1 b b b 1 b b b b 1 b b 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        `),
+    miniMenu.createMenuItem("Caballo", img`
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 b 1 b 1 1 1 1 1 1 
+        1 1 1 1 1 1 b e d e 1 1 1 1 1 1 
+        1 1 1 1 1 e b e e e e 1 1 1 1 1 
+        1 1 1 1 b e b e e e e e 1 1 1 1 
+        1 1 1 1 e c b b b b d 1 1 1 1 1 
+        1 1 1 e e e b b c b b d 1 1 1 1 
+        1 1 1 e e b b b c b b b d 1 1 1 
+        1 1 e e b b b 4 4 b b b d d 1 1 
+        1 1 e e b e b b b b b d d c d 1 
+        1 1 e e e b e b b b b d d d d 1 
+        1 1 e e e b b e e 1 1 b d d d 1 
+        1 1 e e b b b b b 1 1 1 b b 1 1 
+        1 1 e c b b b b b e 1 1 1 1 1 1 
+        1 1 e e b b 4 4 b e 1 1 1 1 1 1 
+        1 1 c b b b 4 4 b b e 1 1 1 1 1 
+        `)
+    ]
+    myMenu = miniMenu.createMenuFromArray(backPack)
+    myMenu.setTitle("Inventario")
 }
 // --- Funciones para animación ---
 function walk_right () {
@@ -117,7 +142,8 @@ function walk_right () {
     true
     )
 }
-let backPack: miniMenu.MenuSprite = null
+let myMenu: miniMenu.MenuSprite = null
+let backPack: miniMenu.MenuItem[] = []
 let woodCutter: Sprite = null
 let vida_arbol = 0
 let golpes = 0
